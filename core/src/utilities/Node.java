833608b4cdbc2f3b81b1fbc70e512ac634c4a563
96kevrud <kevinleerudnick@gmail.com>
2017-03-30 16:39:41 +0200

    Implemented all methods for the Node class

--- core/src/utilities/Node.java
+++ core/src/utilities/Node.java
@@ -1,5 +1,36 @@
 package utilities;
 
-public class Node {
+import java.awt.Point;
+
+public class Node{
+	private float x;
+	private float y;
+	
+	public Node(float x, float y){
+		this.y = y;
+		this.x = x;
+	}
+	
+	public Node(int x , int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Node(Point point){
+		this.x = (float) point.getX();
+		this.y = (float) point.getY();
+	}
+	public void setCord(float x, float y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public float getX(){
+		return x;
+	}
+	
+	public float getY(){
+		return y;
+	}
 
 }
