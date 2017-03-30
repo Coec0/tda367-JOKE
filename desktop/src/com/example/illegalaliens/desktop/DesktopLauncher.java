2354c2985d2cf0ce24dea81df58a6e916ae52c8b
96kevrud <kevinleerudnick@gmail.com>
2017-03-30 20:37:02 +0200

    Changed resolution to 720p

--- desktop/src/com/example/illegalaliens/desktop/DesktopLauncher.java
+++ desktop/src/com/example/illegalaliens/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.example.illegalaliens.IllegalAliensMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 720;
+		config.width = 1280;
 		new LwjglApplication(new IllegalAliensMain(), config);
 	}
 }
