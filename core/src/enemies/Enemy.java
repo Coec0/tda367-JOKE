fa7fb7c4fb2f19195bbd428697a6fb73760b4113
96kevrud <kevinleerudnick@gmail.com>
2017-04-02 16:21:35 +0200

    Fixed the Rotation for enemies

--- core/src/enemies/Enemy.java
+++ core/src/enemies/Enemy.java
@@ -8,7 +8,7 @@ public abstract class Enemy {
 	private float speed;
 	private float health; //Maybe int in future
 	private int nodeArrayPos = 0;
-	private Node direction = new Node(0,-1); //x and y values for the direction start facing upwards
+	private Node direction = new Node(0,1); //x and y values for the start direction. Faces towards east now
 
 	public Enemy(int x, int y, float speed, float health) {
 		this.pos = new SpriteAdapter(x, y);
@@ -23,18 +23,25 @@ public abstract class Enemy {
 		health -= dmg;
 	}
 	
-	public void setDirection(Node newDir){
+	public void setDirection(Node newDir){ 
 		float oldX = direction.getX();
 		float oldY = direction.getY();
 		float newX = newDir.getX();
 		float newY = newDir.getY();
+		
 		float oldLength = (float) Math.sqrt(oldX*oldX + oldY*oldY);
 		float newLength = (float) Math.sqrt(newX*newX + newY*newY);
-		float angle = (float) (Math.acos((oldX*newX + newY*oldY) / (oldLength*newLength)) * (180/Math.PI)); //dot-product devided with length of both vector gives angle
-		System.out.println(angle);
 		
-		pos.rotate(angle);
-		direction = newDir;
+		float angle = (float) (Math.acos((oldX*newX + newY*oldY) / (oldLength*newLength)) * (180/Math.PI)); 
+		//dot-product devided with length of both vector gives the angle
+		
+		float determinant = oldX*newY - oldY*newX; //check the determinat to know if angle is negetiv or not
+		if(determinant < 0){ 
+			pos.rotate(-angle);
+		}else{
+			pos.rotate(angle);
+		}
+		direction = newDir; //update direction
 	}
 	
 	public Node getDirection(){
