f998e62af93898589ad30758525be716a8940ab6
Oscar Carlsson <oscca@student.chalmers.se>
2017-04-20 19:37:15 +0200

    Added Health and Money get/set

--- core/src/main/buildings/WhiteHouse.java
+++ core/src/main/buildings/WhiteHouse.java
@@ -2,9 +2,51 @@ package buildings;
 
 public class WhiteHouse extends Building{
 	private int health;
-	private int money;
+	private float money;
 	
 	public WhiteHouse(String name, int x, int y){
 		super(name, x, y);
+	}
+	
+	public void removeHealth(int amount){
+		health -=amount;
+	}
+	
+	public void removeHealth(){
+		health -=1;
+	}
+	
+	public void setHealth(int amount){
+		health = amount;
+	}
+	
+	public void addHealth(int amount){
+		health += amount;
+	}
+	
+	public void addHealth(){
+		health += 1;
+	}
+	
+	public int getHealth(){
+		return health;
+	}
+	
+	
+	
+	public void addMoney(float amount){
+		money += amount;
+	}
+	
+	public void setMoney(float amount){
+		money = amount;
+	}
+	
+	public void removeMoney(float amount){
+		money -= amount;
+	}
+	
+	public float getMoney(){
+		return money;
 	}
 }
