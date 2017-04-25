74db66fb90716a372348ba73ecacce68df73d1f0
Oscar Carlsson <oscca@student.chalmers.se>
2017-04-25 16:47:09 +0200

    Button now theoreticly works, but wont do anything and isnt visible

--- core/src/main/ui/UIButton.java
+++ core/src/main/ui/UIButton.java
@@ -3,14 +3,35 @@ package ui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.utils.Array;
 
-public class UIButton {
+public class UIButton extends InputAdapter{
 
-	public UIButton() {
-
+	private int x;
+	private int y;
+	
+	private int width;
+	private int height;
+	
+	
+	public UIButton(int x, int y) {
+		this.setX(x);
+		this.setY(y);
+	}
+	
+	public UIButton(int x, int y, int width, int height){
+		this(x,y);
+		this.setHeight(height);
+		this.setWidth(width);
 	}
 
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		if(x<screenX && y<screenY && y+height>screenY && x+width>screenX)
+			System.out.println("HIT");
+		return super.touchDown(screenX, screenY, pointer, button);
+	}
 	
 	private Array<ActionListener> aListeners = new Array<ActionListener>(false, 10);
 	
@@ -25,6 +46,38 @@ public class UIButton {
 	private void notifyListeners(ActionEvent e) {
 		for (ActionListener l : aListeners)
 			l.actionPerformed(e);
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
 	}
 
 }
