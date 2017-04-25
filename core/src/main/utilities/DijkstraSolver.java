578521071d9451ffa7a1d686bd1e7b84485ec5da
96kevrud <kevinleerudnick@gmail.com>
2017-04-25 23:01:58 +0200

    Implemented dijkstra. I think i found a bug where it doesent always calc shortestpath, but too tired to find it tonight :P

--- core/src/main/utilities/DijkstraSolver.java
+++ core/src/main/utilities/DijkstraSolver.java
@@ -4,12 +4,102 @@ import com.badlogic.gdx.utils.Array;
 
 public class DijkstraSolver {
 	private Array<MapNode> allNodes;
-	private MapNode vertex;
+	private MapNode startNode;
+	private MapNode endNode;
+	private MapNode currentNode;
+	private boolean searching = true;
 	
-	public DijkstraSolver(Array<MapNode> allNodes, MapNode vertex){
+	public DijkstraSolver(Array<MapNode> allNodes){
 		this.allNodes = allNodes;
-		this.vertex = vertex;
+	}
+	
+	public Array<Node> solve(MapNode startNode, MapNode endNode){
+		
+		this.startNode = startNode;
+		this.endNode = endNode;
+		startNode.setPathLenght(0);
+		setCurrentNode(startNode);
+				
+		weighNeighbors();
+		while(true){
+			if(pickNewCurrentNode()){
+				break;
+			}
+			weighNeighbors();
+			System.out.println("Ogge");
+			
+		}
+		return findPath();
+	}
+	
+	private Array<Node> findPath(){
+		MapNode tmpNode = endNode;
+		Array<Node> path = new Array<Node>();
+		String prevID;
+		
+		while(tmpNode != startNode){
+			System.out.println("Kevin");
+			path.add(tmpNode.getPos());
+			prevID = tmpNode.getPrevID();
+			tmpNode = findNode(prevID);
+		}
+		path.add(startNode.getPos());
+		path.reverse();
+		return path;	
 	}
 	
 	
+	private void weighNeighbors(){
+		MapNode neighbor;
+		for(String ID : currentNode.getNeighbors()){
+			neighbor = findNode(ID);
+			weighNeighbor(neighbor);	
+		}
+	}
+	
+	private boolean pickNewCurrentNode(){
+		MapNode neighbor;
+		MapNode prospect = currentNode; //just to have a starting value
+		float minPathLength = Float.MAX_VALUE;
+		for(String ID : currentNode.getNeighbors()){
+			System.out.println("whiskey");
+			neighbor = findNode(ID);
+			if( !neighbor.Visited()){	//neighbor.getPathLenght() < minPathLength && 
+				minPathLength = neighbor.getPathLenght();
+				prospect = neighbor;
+				
+				
+			}
+			
+		}
+		
+		if(prospect.equals(currentNode)){
+			return true;
+		}
+		
+		setCurrentNode(prospect);
+		return false;
+	}
+	
+	private void weighNeighbor(MapNode node){
+		if(currentNode.getPathLenght() + currentNode.getDistanceTo(node) < node.getPathLenght()){
+			node.setPathLenght(currentNode.getPathLenght() + currentNode.getDistanceTo(node));
+			node.setPrevID(currentNode.getID());
+		}
+		
+	}
+
+	private MapNode findNode(String ID){
+		for(MapNode MN : allNodes){
+			if(MN.getID().equals(ID)){
+				return MN;
+			}
+		}
+		return null;
+	}
+	
+	private void setCurrentNode(MapNode node){
+		currentNode = node;
+		node.visit();
+	}
 }
