e5fb58dbc9c479e766ffa053439dc8e86a8a021f
Oscar Carlsson <oscca@student.chalmers.se>
2017-04-26 13:16:02 +0200

    This isnt used anymore, prob gonna remove

--- core/src/main/ui/HUD.java
+++ core/src/main/ui/HUD.java
@@ -12,13 +12,13 @@ import views.HUDView;
 public class HUD {
 	Array<UIButton> buttons = new Array<UIButton>(false, 10);
 	InputMultiplexer IM;
-	HUDView HW = new HUDView();
+	//HUDView HW = new HUDView();
 	
 	public HUD(InputMultiplexer IM){
 		this.IM = IM;
 		UIButton nextWave = new UIButton(Gdx.graphics.getWidth()-100,Gdx.graphics.getHeight()-70, 100, 70, "nextWave", "Send next wave");
 		addButton(nextWave);
-		HW.addToView(new TextArea(nextWave.getX(),Gdx.graphics.getHeight()-nextWave.getY(), nextWave.getWidth(), nextWave.getHeight(), nextWave.getText()));
+		//HW.addToView(new TextArea(nextWave.getX(),Gdx.graphics.getHeight()-nextWave.getY(), nextWave.getWidth(), nextWave.getHeight(), nextWave.getText()));
 	}
 	
 	private void addButton(UIButton button){
