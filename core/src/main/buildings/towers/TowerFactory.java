2e106b57e0cdf1c482bd560c9db74be746c42663
Oscar Carlsson <oscca@student.chalmers.se>
2017-04-26 17:29:42 +0200

    Removed Y-axis fix from TowerFactory

--- core/src/main/buildings/towers/TowerFactory.java
+++ core/src/main/buildings/towers/TowerFactory.java
@@ -1,16 +1,15 @@
 package buildings.towers;
 
-import com.badlogic.gdx.Gdx;
 import controllers.ProjectileController;
 
 public class TowerFactory {
 	public static Tower createSoldier(int x, int y, ProjectileController PController) {
-		return new Soldier(x, Gdx.graphics.getHeight() - y, PController);
+		return new Soldier(x, y, PController);
 
 	}
 
 	public static Tower createTank(int x, int y, ProjectileController PController) {
-		return new Tank(x, Gdx.graphics.getHeight() - y, PController);
+		return new Tank(x, y, PController);
 	}
 
 }
