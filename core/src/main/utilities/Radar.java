15d06aab038e3b4493ba5f6be74c652a0f0a60bc
Johan Svennungsson <johan.svennungsson@gmail.com>
2017-05-01 23:59:21 +0200

    some refactoring with added javadoc-comments

--- core/src/main/utilities/Radar.java
+++ core/src/main/utilities/Radar.java
@@ -8,41 +8,38 @@ import models.AlienModel;
 public class Radar {
 	
 	private AlienModel aModel;
-	
-	
+
 	public Radar(AlienModel aModel){
 		this.aModel = aModel;
 	}
 
-	
+	/**
+	 * Scans for all Enemies known by AlienModel.
+	 * @param center
+	 * @param radius
+	 * @return array of known enemies
+	 */
 	public Array<Enemy> scan(Node center, float radius){
 		Array<Enemy> knownAliens = new Array<Enemy>();
 		
-		for(Enemy alien : aModel.getAllAliens()){
-			float distance = (float) center.getDistanceTo(alien.getPos());
-			if(distance <= radius+alien.getRadius()){
+		for(Enemy alien : aModel.getAllAliens()) {
+			if (isEnemyWithinRadius(center, alien, radius)) {
 				knownAliens.add(alien);
 			}
 		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*for(Alien alien : aModel.getAllAliens()){
-			float deltaX = alien.getPos().getX() - center.getX();
-	    	float deltaY = alien.getPos().getY() - center.getY();
-	    	
-	    	if((deltaX*deltaX) + (deltaY*deltaY) < radius*radius){
-	    		knownAliens.add(alien);
-	    	}
-		}*/
-		
+
 		return knownAliens;
     }
+
+	/**
+	 * Checks if Alien is within radius from Node. All Aliens it's own radius because of Texture.
+	 * @param center
+	 * @param alien
+	 * @param radius
+	 * @return true if within radius
+	 */
+    public boolean isEnemyWithinRadius(Node center, Enemy alien, float radius) {
+		return center.getDistanceTo(alien.getPos()) <= radius + alien.getRadius();
+	}
 
 }
