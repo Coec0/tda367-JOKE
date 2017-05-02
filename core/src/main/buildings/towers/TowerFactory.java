0f244759f3568f1a973599a5b9943dc76edf094a
Oscar Carlsson <oscca@student.chalmers.se>
2017-05-02 22:07:09 +0200

    TowerFactory now returns its towers better

--- core/src/main/buildings/towers/TowerFactory.java
+++ core/src/main/buildings/towers/TowerFactory.java
@@ -1,24 +1,24 @@
 package buildings.towers;
 
-import controllers.ProjectileController;
-
 public class TowerFactory {
-	public static Tower createSoldier(int x, int y) {
+	public static Soldier createSoldier(int x, int y) {
 		return new Soldier(x, y);
 	}
 
-	public static Tower createTank(int x, int y) {
+	public static Tank createTank(int x, int y) {
 		return new Tank(x, y);
 	}
 
-    public static Tower createSniper(int x, int y) {
-        return new Sniper(x, y);
-    }
-    public static Tower createRanger(int x, int y) {
-        return new Ranger(x, y);
-    }
-    public static Tower createHowitzer(int x, int y) {
-        return new Howitzer(x, y);
-    }
+	public static Sniper createSniper(int x, int y) {
+		return new Sniper(x, y);
+	}
+
+	public static Ranger createRanger(int x, int y) {
+		return new Ranger(x, y);
+	}
+
+	public static Howitzer createHowitzer(int x, int y) {
+		return new Howitzer(x, y);
+	}
 
 }
