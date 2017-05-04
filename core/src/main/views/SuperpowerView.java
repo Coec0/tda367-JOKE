5f1b678b3a1ef769901e506ee2a6afa822f965aa
Emil <silvervibe@hotmail.com>
2017-05-04 23:08:20 +0200

    Forgot to add a file

--- core/src/main/views/SuperpowerView.java
+++ core/src/main/views/SuperpowerView.java
@@ -2,7 +2,6 @@ package views;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Circle;
-import superpowers.Superpower;
 import utilities.DrawablesCollector;
 import utilities.SpriteAdapter;
 
