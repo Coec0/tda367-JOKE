08d3cf3ed32fd9d9091ff9db57a9fcdeb357520f
Johan Svennungsson <johan.svennungsson@gmail.com>
2017-05-04 19:30:27 +0200

    After Radar refactor, RadarTest can finally be testable.

--- core/src/test/utilities/RadarTest.java
+++ core/src/test/utilities/RadarTest.java
@@ -4,7 +4,7 @@ import buildings.towers.Soldier;
 import com.badlogic.gdx.utils.Array;
 import enemies.Alien;
 import enemies.Enemy;
-import models.AlienModel;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,24 +13,30 @@ import static org.junit.Assert.*;
  * @author Johan Svennungsson
  */
 public class RadarTest {
-	
-	//private PathFinder finder = new PathFinder();
-    /*private AlienModel alienModel = new AlienModel(finder);
-    private Radar radar = new Radar(alienModel);
-    private Soldier soldier = new Soldier(100, 100);
-    private Alien alien = new Alien();
 
+    private Array<Enemy> enemyArray;
+    private Radar radar;
+    private Soldier soldier;
+    private Alien alien;
+
+    @Before
+    public void setUp() throws Exception {
+        enemyArray = new Array<Enemy>();
+        radar = new Radar();
+        soldier = new Soldier(100,100);
+        alien = new Alien();
+
+        enemyArray.add(alien);
+    }
     @Test
     public void scan() throws Exception {
-        alien.setPos(new Node(150,150));
-        alienModel.addAlien(alien);
-
+        alien.setPos(150,150);
         //Distance between soldier and Alien is 70.71, but the Alien's radius is 10. 70.71 - 10 = 60.61
         float failRadius = 60;
         float successRadius = 61;
 
-        Array<Enemy> aliensNotFound = radar.scan(soldier.getPos(), failRadius);
-        Array<Enemy> aliensFound = radar.scan(soldier.getPos(), successRadius);
+        Array<Enemy> aliensNotFound = radar.scan(soldier.getPos(), failRadius, enemyArray);
+        Array<Enemy> aliensFound = radar.scan(soldier.getPos(), successRadius, enemyArray);
 
         assertEquals(aliensNotFound.size, 0);
         assertFalse(aliensNotFound.contains(alien, false));
@@ -41,31 +47,28 @@ public class RadarTest {
 
     @Test
     public void scanNegative() throws Exception {
-        alien.setPos(new Node(-10, -10));
-        alienModel.addAlien(alien);
+        alien.setPos(-10,-10);
 
         //Distance between soldier and Alien is 155.56, but the Alien's radius is 10. 155.56 - 10 = 145.56
         float failRadius = 145;
         float successRadius = 146;
 
-        Array<Enemy> aliensNotFound = radar.scan(soldier.getPos(), failRadius);
-        Array<Enemy> aliensFound = radar.scan(soldier.getPos(), successRadius);
+        Array<Enemy> aliensNotFound = radar.scan(soldier.getPos(), failRadius, enemyArray);
+        Array<Enemy> aliensFound = radar.scan(soldier.getPos(), successRadius, enemyArray);
 
         assertEquals(aliensNotFound.size, 0);
-        assertFalse(aliensNotFound.contains(alien, false));
+        assertEquals(aliensNotFound.contains(alien, false), false);
 
         assertEquals(aliensFound.get(0), alien);
-        assertTrue(aliensFound.contains(alien, false));
+        assertEquals(aliensFound.contains(alien, false), true);
     }
 
     @Test
     public void isEnemyWithinRadius() throws Exception {
-        alien.setPos(new Node(150,150));
-        alienModel.addAlien(alien);
+        alien.setPos(150,150);
 
         float distance = (float) soldier.getPos().getDistanceTo(alien.getPos());
 
         assertEquals(distance, 70.71, 0.01);
     }
-*/
 }
