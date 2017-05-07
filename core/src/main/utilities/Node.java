48fe220df76566b747b805c8a7b12da5944175b4
96kevrud <kevinleerudnick@gmail.com>
2017-05-07 17:37:13 +0200

    Added equals and getHashcode to Node class

--- core/src/main/utilities/Node.java
+++ core/src/main/utilities/Node.java
@@ -11,6 +11,31 @@ public class Node{
 		this.x = x;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(x);
+		result = prime * result + Float.floatToIntBits(y);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Node other = (Node) obj;
+		if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x))
+			return false;
+		if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y))
+			return false;
+		return true;
+	}
+
 	public Node(int x , int y){
 		this.x = x;
 		this.y = y;
