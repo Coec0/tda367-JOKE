263c9cb2bc0ce0e008f11cd0de0f6a37520d2286
Johan Svennungsson <johan.svennungsson@gmail.com>
2017-05-08 12:19:24 +0200

    added test class for Map and MapParser. Keep in mind you need to set working directory to android/assets/ for the tests to pass. Can't find a better solution for now.

---
+++ core/src/test/map/MapParserTest.java
@@ -0,0 +1,42 @@
+package map;
+
+import com.badlogic.gdx.utils.Array;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test class for MapParser.
+ * @author Johan Svennungsson
+ */
+public class MapParserTest {
+
+	private MapParser mapParser;
+
+	@Before
+	public void setUp() throws Exception {
+		mapParser = new MapParser("AlphaMap");
+	}
+
+	@Test
+	public void hasNextLine() throws Exception {
+		//24 lines in AlphaMapNodes.txt
+		for (int i = 0 ; i < 24; i++) {
+			assertEquals(mapParser.hasNextLine(), true);
+			mapParser.getParsedLine();
+		}
+		assertEquals(mapParser.hasNextLine(), false);
+	}
+
+	@Test
+	public void getParsedLine() throws Exception {
+		//A;0;570;E
+		Array<String> parsedLine = mapParser.getParsedLine();
+		assertEquals(parsedLine.get(0), "A");
+		assertEquals(parsedLine.get(1), "0");
+		assertEquals(parsedLine.get(2), "570");
+		assertEquals(parsedLine.get(3), "E");
+	}
+
+}
---
+++ core/src/test/map/MapTest.java
@@ -0,0 +1,65 @@
+package map;
+
+import com.badlogic.gdx.utils.Array;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test class for Map
+ * @author Johan Svennungsson
+ */
+public class MapTest {
+
+	private Map map;
+
+	@Before
+	public void setUp() throws Exception {
+		map = new Map("AlphaMap");
+	}
+
+	@Test
+	public void getStartingNodes() throws Exception {
+		assertEquals(map.getStartingNodes().get(0).getID(), "A");
+		assertEquals(map.getStartingNodes().get(0).getPos().getX(), 0, 0);
+		assertEquals(map.getStartingNodes().get(0).getPos().getY(), 570, 0);
+		assertEquals(map.getStartingNodes().get(0).getNeighbors().get(0), "E");
+	}
+
+	@Test
+	public void addMapNodes() throws Exception {
+		map.addMapNodes(new MapParser("AlphaMap"));
+		assertEquals(map.getMapNodes().size, 24);
+
+		map.getMapNodes().clear();
+		assertEquals(map.getMapNodes().size, 0);
+	}
+
+	@Test
+	public void convertLineToMapNode() throws Exception {
+		Array<String> segments = new Array<String>();
+		segments.addAll("Y", "593", "1200", "E", "K");
+
+		MapNode mapNode = map.convertLineToMapNode(segments);
+
+		assertEquals(mapNode.getID(), "Y");
+		assertEquals(mapNode.getPos().getX(), 593, 0);
+		assertEquals(mapNode.getPos().getY(), 1200, 0);
+		assertEquals(mapNode.getNeighbors().get(0), "E");
+		assertEquals(mapNode.getNeighbors().get(1), "K");
+		assertEquals(mapNode.getNeighbors().size, 2);
+	}
+
+	@Test
+	public void getMapNodes() throws Exception {
+		assertEquals(map.getMapNodes().size, 24);
+	}
+
+	@Test
+	public void getTexture() throws Exception {
+		//Cannot instantiate Texture due to Gdx.files.internal
+		assertNull(map.getTexture());
+	}
+
+}
