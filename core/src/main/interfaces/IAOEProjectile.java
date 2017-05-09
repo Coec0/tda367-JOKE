d7e51c03b03692573933d2931c3d793b5a8de808
knya <johsvenn@student.chalmers.se>
2017-05-09 18:28:20 +0200

    moved existing interface to subpackage .interfaces

---
+++ core/src/main/interfaces/IAOEProjectile.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+/**
+ * Created by Emil on 2017-05-03.
+ */
+public interface IAOEProjectile extends IProjectile {
+    float getAOERadius();
+    void setAOERadius(float aoeRadius);
+}
