3610acba70de4981cfcc5a231539b52ab4cd2e70
Oscar Carlsson <oscca@student.chalmers.se>
2017-05-10 13:14:27 +0200

    Added equals and addVotes to Party

--- core/src/main/politics/parties/Party.java
+++ core/src/main/politics/parties/Party.java
@@ -16,6 +16,35 @@ public class Party {
 	public void setVotes(int votes) {
 		this.votes = votes;
 	}
+	
+	public void addVotes(int votes){
+		setVotes(this.votes+votes);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Party other = (Party) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 
 	public String getName() {
 		return name;
