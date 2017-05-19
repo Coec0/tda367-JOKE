0a0a75de7945264c68ca8bdd9522e5e64fba1cae
Oscar Carlsson <oscca@student.chalmers.se>
2017-05-19 23:00:36 +0200

    Semi added declare war, but cant use it yet

--- core/src/main/models/ExecutiveOrdersModel.java
+++ core/src/main/models/ExecutiveOrdersModel.java
@@ -1,20 +1,29 @@
 package models;
 
+import cooldown.WavesCDHandler;
+import cooldown.WavesCooldown;
 import executive_orders.CivilWar;
 import executive_orders.ExecutiveOrder;
+import executive_orders.DeclareWar;
 import politics.parties.Party;
 import politics.parties.PartyFactory;
+import towers.BOPrototypes;
 
 public class ExecutiveOrdersModel {
 
-	ExecutiveOrder CWR, CWD;
-	Party republican, democrat;
+	private ExecutiveOrder CWR, CWD;
+	private DeclareWar DW;
+	private Party republican, democrat;
+	private WavesCDHandler wcd;
 	
-	public ExecutiveOrdersModel(BuildingModel BM){
+	public ExecutiveOrdersModel(BuildingModel BM, WavesCDHandler wcd, BOPrototypes prots){
+		this.wcd = wcd;
 		republican = PartyFactory.Republican(0);
 		democrat = PartyFactory.Democrat(0);
 		CWD = new CivilWar(BM, republican, democrat);
 		CWR = new CivilWar(BM, democrat, republican);
+		DW = new DeclareWar(BM.getWhiteHouses().peek(), 30000, republican, prots);
+		wcd.addCooldownObject(DW);
 	}
 	
 	
