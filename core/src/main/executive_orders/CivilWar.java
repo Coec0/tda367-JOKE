c5ee60d0d8f35bb3e4e871dc95839420a8dd92d9
Oscar Carlsson <oscca@student.chalmers.se>
2017-05-19 20:09:53 +0200

    Updated CivilWar

--- core/src/main/executive_orders/CivilWar.java
+++ core/src/main/executive_orders/CivilWar.java
@@ -5,34 +5,36 @@ import models.BuildingModel;
 import politics.parties.Party;
 
 public class CivilWar implements ExecutiveOrder{
-	BuildingModel BM;
-	String killParty;
-	private Party party;
-	public CivilWar(BuildingModel BM, String killParty, Party party){
+	private BuildingModel BM;
+	private Party killParty;
+	private Party killerParty;
+	public CivilWar(BuildingModel BM, Party killerParty, Party killParty){
 		this.BM  = BM;
 		this.killParty = killParty;
-		this.party= party;
+		this.killerParty = killerParty;
 	}
 	
 	@Override
 	public void execute() {
+		int points= 0;
 		for(BoardObject BO : BM.getAllBoardObjects()){
-			if(BO.getParty() != null && BO.getParty().equals(party)){
+			if(BO.getParty() != null && BO.getParty().equals(killParty)){
+				points += BO.getParty().getPoints();
 				BM.sellBoardObject(BO, false);
 			}
 		}
-		
+		BM.getWhiteHouses().peek().voteParty(new Party(killerParty.getName(), 0, points));
 	}
 
 	@Override
 	public String getDescription() {
-		return "Kills every "+ party.getName() +" tower and building. Rewards you with"
+		return "Kills every "+ killParty.getName() +" tower and building. Rewards you with"
 				+ " alot of political points.";
 	}
 
 	@Override
 	public Party getParty() {
-		return party;
+		return killerParty;
 	}
 
 }
