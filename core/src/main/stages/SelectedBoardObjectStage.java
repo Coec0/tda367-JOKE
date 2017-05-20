070501d216241e83202421c4b9b978d5b4c9f3c0
Johan Svennungsson <johan.svennungsson@gmail.com>
2017-05-21 01:05:56 +0200

    added SelectedBoardObjectStage

---
+++ core/src/main/stages/SelectedBoardObjectStage.java
@@ -0,0 +1,49 @@
+package stages;
+
+
+import buildings.BoardObject;
+import buildings.Building;
+import com.badlogic.gdx.InputMultiplexer;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import towers.Tower;
+import utilities.DrawablesCollector;
+
+/**
+ * The Stage for showing additional information about a selected BoardObject.
+ * @author Johan Svennungsson
+ */
+public class SelectedBoardObjectStage extends AbstractStage {
+
+	private DrawablesCollector DC = DrawablesCollector.getInstance();
+
+	private SelectedBuildingStage selectedBuildingStage;
+	private SelectedTowerStage selectedTowerStage;
+
+	static final float stageWidth = 200;
+
+	protected SelectedBoardObjectStage() {}
+
+	public SelectedBoardObjectStage(InputMultiplexer inputMultiplexer, ClickListener clickListener) {
+		selectedBuildingStage = new SelectedBuildingStage(clickListener);
+		selectedTowerStage = new SelectedTowerStage(clickListener);
+
+		inputMultiplexer.addProcessor(selectedBuildingStage);
+		inputMultiplexer.addProcessor(selectedTowerStage);
+	}
+
+	public void setBoardObjectStage(BoardObject boardObject) {
+		if (boardObject instanceof Tower && boardObject.isActive()) {
+			DC.addStage(selectedTowerStage);
+			selectedTowerStage.setTower((Tower) boardObject);
+		} else {
+			DC.removeStage(selectedTowerStage);
+		}
+
+		if (boardObject instanceof Building) {
+			DC.addStage(selectedBuildingStage);
+			selectedBuildingStage.setBuilding(boardObject);
+		} else {
+			DC.removeStage(selectedBuildingStage);
+		}
+	}
+}
