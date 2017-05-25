3dd9a91b62d744c29f10609512610b5776faaff4
Johan Svennungsson <johan.svennungsson@gmail.com>
2017-05-26 00:03:10 +0200

    StageSwitcher now doesn't hold any stages, only switches between them

--- core/src/main/stages/StageSwitcher.java
+++ core/src/main/stages/StageSwitcher.java
@@ -3,70 +3,35 @@ package stages;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.utils.Array;
-import com.example.illegalaliens.IllegalAliensMain;
 import interfaces.IStage;
 
 /**
  * Class for switching between Stages in MainMenuScreen
  * @author Johan Svennungsson
  */
-public class StageSwitcher extends AbstractStage {
+public class StageSwitcher {
 
-	private Array<IStage> stages = new Array<IStage>();
-
-	private MainMenuStage mainMenuStage;
-	private AboutStage aboutStage;
-	private MapSelectStage mapSelectStage;
-
-	public StageSwitcher(IllegalAliensMain game) {
-		mainMenuStage = new MainMenuStage(game, this);
-		aboutStage = new AboutStage(game, this);
-		mapSelectStage = new MapSelectStage(game, this);
-
-		stages.addAll(mainMenuStage, aboutStage, mapSelectStage);
-	}
-
-	public void drawStages() {
-		for (IStage stage : stages) {
-			stage.draw();
-		}
-	}
+	private IStage previousStage;
 
 	/**
 	 * Show a stage.
 	 * @param stage
 	 */
-	public void showStage(IStage stage){
+	public void showStage(IStage stage) {
+		this.hidePreviousStage();
+
 		for(Actor actor : stage.getActors()){
 			actor.setVisible(true);
 		}
 
-		this.hideAllStagesExcept(stage);
+		previousStage = stage;
 
 		Gdx.input.setInputProcessor((InputProcessor) stage);
 	}
 
-	/**
-	 * Hide all stages except the one specified.
-	 * @param s
-	 */
-	private void hideAllStagesExcept(IStage s) {
-		for (IStage stage : stages) {
-			stage.setVisible(false);
+	private void hidePreviousStage() {
+		if (previousStage != null) {
+			previousStage.setVisible(false);
 		}
-		s.setVisible(true);
-	}
-
-	public IStage getMainMenuStage() {
-		return mainMenuStage;
-	}
-
-	public IStage getAboutStage() {
-		return aboutStage;
-	}
-
-	public IStage getMapSelectStage() {
-		return mapSelectStage;
 	}
 }
