302b30225827ddf4f0b23ac3edc2aa126617aecb
Emil <silvervibe@hotmail.com>
2017-05-28 23:10:51 +0200

    Changed UI panel color.

--- core/src/main/com/example/illegalaliens/screens/GameScreen.java
+++ core/src/main/com/example/illegalaliens/screens/GameScreen.java
@@ -171,7 +171,7 @@ public class GameScreen implements Screen{
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(212f/255f, 212f/255f, 212f/255f, 0);
+		Gdx.gl.glClearColor(140f/255f, 140f/255f, 140f/255f, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		VP.apply();
