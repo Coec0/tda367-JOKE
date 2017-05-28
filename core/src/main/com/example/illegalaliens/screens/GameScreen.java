2838d84abc86dcb87f5cb5b3a3790b1dee3fc4f8
Emil <silvervibe@hotmail.com>
2017-05-28 12:00:13 +0200

    Changed ui panel color

--- core/src/main/com/example/illegalaliens/screens/GameScreen.java
+++ core/src/main/com/example/illegalaliens/screens/GameScreen.java
@@ -183,7 +183,7 @@ public class GameScreen implements Screen{
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(148f/255f, 204f/255f, 233f/255f, 0);
+		Gdx.gl.glClearColor(212f/255f, 212f/255f, 212f/255f, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		WP.apply();
